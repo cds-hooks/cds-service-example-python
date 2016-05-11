6e3cb376876217f5660aea76ffa677f3b0cbc065
Josh Mandel <jmandel@alum.mit.edu>
2016-05-10 22:39:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask.ext.cors import CORS
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route('/.well-known/cds-services')
+@app.route('/cds-services')
 def discovery():
   return json.jsonify({
     'services': [
